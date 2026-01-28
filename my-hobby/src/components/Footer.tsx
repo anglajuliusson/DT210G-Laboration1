a3d5cacbab1f846cd418db57c03241a9623b8882
Ängla Juliusson <angla02@icloud.com>
2026-01-28 13:32:20 +0100

    Footer komponent

---
+++ my-hobby/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>DT210G - Laboration 1 | anju2402, Ängla Juliusson | 2026</p>
+        </footer>
+    )
+}
+
+export default Footer;
