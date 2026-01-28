9075988b2e648c18fdc614839ea8db51eee136b0
Ängla Juliusson <angla02@icloud.com>
2026-01-28 15:02:15 +0100

    Styling för Header-komponent

--- my-hobby/src/components/Header.tsx
+++ my-hobby/src/components/Header.tsx
@@ -2,14 +2,24 @@ import bannerAvif from "../assets/images/banner.avif"
 import bannerWebp from "../assets/images/banner.webp"
 import bannerJpg from "../assets/images/banner.jpg"
 
+const myStyle = {
+    backgroundColor: "red",
+    color: "white",
+    padding: "2rem",
+}
+const imgStyle = {
+    borderRadius: "10px",
+    width: "100%",
+}
+
 function Header() {
     return (
-        <header>
-            <h1>Mitt fritidsintresse</h1>
+        <header style={myStyle}>
+            <h1 style={{ marginBottom: "1em", textAlign: "center" }}>Mitt fritidsintresse</h1>
             <picture>
                 <source srcSet={bannerAvif} type="image/avif" />
                 <source srcSet={bannerWebp} type="image/webp" />
-                <img src={bannerJpg} alt="Bannerbild - Kent konsert" className="banner_img" />
+                <img style={imgStyle} src={bannerJpg} alt="Bannerbild - Kent konsert" className="banner_img" />
             </picture>
         </header>
     )
