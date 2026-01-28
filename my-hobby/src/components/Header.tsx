1bc7f7d317815edb1f8b03a53a8bfa81d441c044
Ängla Juliusson <angla02@icloud.com>
2026-01-28 14:31:13 +0100

    Inkludera banner bild och skriv ut i headerkomponenten

--- my-hobby/src/components/Header.tsx
+++ my-hobby/src/components/Header.tsx
@@ -1,7 +1,16 @@
+import bannerAvif from "../assets/images/banner.avif"
+import bannerWebp from "../assets/images/banner.webp"
+import bannerJpg from "../assets/images/banner.jpg"
+
 function Header() {
     return (
         <header>
             <h1>Mitt fritidsintresse</h1>
+            <picture>
+                <source srcSet={bannerAvif} type="image/avif" />
+                <source srcSet={bannerWebp} type="image/webp" />
+                <img src={bannerJpg} alt="Bannerbild - Kent konsert" className="banner_img" />
+            </picture>
         </header>
     )
 }
