65710851598c6e51827c331ee536b15b924f8174
Ängla Juliusson <angla02@icloud.com>
2026-01-28 15:07:34 +0100

    Styling för Footerkomponent

--- my-hobby/src/components/Footer.tsx
+++ my-hobby/src/components/Footer.tsx
@@ -1,7 +1,13 @@
+const myStyle = {
+    backgroundColor: "red",
+    color: "white",
+    padding: "2rem",
+}
+
 function Footer() {
     return(
-        <footer>
-            <p>DT210G - Laboration 1 | anju2402, Ängla Juliusson | 2026</p>
+        <footer style={myStyle}>
+            <p style={{ textAlign: "center" }}>DT210G - Laboration 1 | anju2402, Ängla Juliusson | 2026</p>
         </footer>
     )
 }
