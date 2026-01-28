38b935d558e16b7701060ec6ba10432a5bce6fce
Ängla Juliusson <angla02@icloud.com>
2026-01-28 15:36:32 +0100

    Implementerar header-komponent med props och inline-styling

--- my-hobby/src/App.tsx
+++ my-hobby/src/App.tsx
@@ -1,14 +1,18 @@
+// Importera sidans huvudkomponenter
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Conserts from "./components/Conserts"
-import "./assets/css/App.css"
+import "./assets/css/App.css" // Importera generell CSS för applikationen
 
 function App() {
 
   return (
     <>
-      <Header />
+      {/* Header-komponenten får sidans titel skickad som prop */}
+      <Header title="Mitt fritidsintresse" />
+      {/* Huvudinnehåll med dynamisk genererad lista */}
       <Conserts />
+      {/* Footer-komponent */}
       <Footer />
     </>
   )
--- my-hobby/src/components/Header.tsx
+++ my-hobby/src/components/Header.tsx
@@ -1,21 +1,32 @@
+// Importera bilder från assets-mappen
 import bannerAvif from "../assets/images/banner.avif"
 import bannerWebp from "../assets/images/banner.webp"
 import bannerJpg from "../assets/images/banner.jpg"
 
+// Typdefinition för props till Header-komponenten
+// Titeln ska skickas från App-komponenten
+type HeaderProps = {
+    title: string
+  }
+
+// Inline-styling för header (komponentspecifik CSS)
 const myStyle = {
     backgroundColor: "red",
     color: "white",
     padding: "2rem",
 }
+
+// Inline-styling för bannerbilden
 const imgStyle = {
     borderRadius: "10px",
     width: "100%",
 }
 
-function Header() {
+// Header-komponenten tar emot title som prop
+function Header({title}: HeaderProps) {
     return (
         <header style={myStyle}>
-            <h1 style={{ marginBottom: "1em", textAlign: "center" }}>Mitt fritidsintresse</h1>
+            <h1 style={{ marginBottom: "1em", textAlign: "center" }}>{title}</h1>
             <picture>
                 <source srcSet={bannerAvif} type="image/avif" />
                 <source srcSet={bannerWebp} type="image/webp" />
